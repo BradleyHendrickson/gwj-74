3aa25d02e338409cfdaeddc2767a90db76143eef
Brad Hendrickson <bradley.hendrickson6@gmail.com>
2024-10-11 20:01:31 -0700

    init game

---
+++ scenes/gameplay/characters/player/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var speed = 200
+@export var friction = 0.099
+@export var acceleration = 0.2
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('move_right'):
+		input.x += 1
+	if Input.is_action_pressed('move_left'):
+		input.x -= 1
+	if Input.is_action_pressed('move_down'):
+		input.y += 1
+	if Input.is_action_pressed('move_up'):
+		input.y -= 1
+	return input
+
+func _physics_process(delta):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	move_and_slide()
