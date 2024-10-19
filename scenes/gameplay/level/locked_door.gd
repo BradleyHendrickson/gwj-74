2bac2b1def692a784b211c629e648f8cbc4b8bbf
Sparkplugx <Hendricksonmax123@gmail.com>
2024-10-19 12:24:30 -0700

    added door opening

--- scenes/gameplay/level/locked_door.gd
+++ scenes/gameplay/level/locked_door.gd
@@ -1,5 +1,28 @@
 extends StaticBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+func playsideclose():
+	$AnimatedSprite2D.play("Sideclose")
+
+func playtopclose():
+	$AnimatedSprite2D.play("Topclose")
+	
+func playside():
+	$AnimatedSprite2D.play("Sideclose")
+
+func playtop():
+	$AnimatedSprite2D.play("Topclose")
+	
+func setAnimation(animation):
+	animated_sprite_2d.animation = animation
+	if animation == "Side":
+		collision_shape_2d.rotation = deg_to_rad(90)
+		
+
+func setFlip(flip : bool):
+	animated_sprite_2d.flip_h = flip
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
